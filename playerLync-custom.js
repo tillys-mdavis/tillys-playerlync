820325edee7aec2dca9890f8fb16dde11b693ac6
tillys-mdavis <142350178+tillys-mdavis@users.noreply.github.com>
2023-10-20 15:34:09 -0700

    Update playerLync-custom.js

--- playerLync-custom.js
+++ playerLync-custom.js
@@ -1,1 +1,15 @@
 console.log("MIKES TEST HOSTED JS SCRIPT V2");
+
+$( document ).ready(function() {	
+
+	$("input, textarea").each(function() {
+	  $(this).keypress(function(event) {
+		if (event.which == 34) {
+		  event.preventDefault();
+		  $(this).val($(this).val() + "”");
+		  //console.log("you pressed double quote");
+		}	
+	  });
+	});
+	
+});
